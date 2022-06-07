80a51adfc2c8776053d3784d9faf714a31dc75c8
bueinat <62245924+bueinat@users.noreply.github.com>
2022-06-07 20:53:17 +0300

    dragging enabled

--- Views/UserControls/NavigationTree.xaml.cs
+++ Views/UserControls/NavigationTree.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace mouse_tracking_web_app.Views
 {
@@ -10,6 +12,18 @@ namespace mouse_tracking_web_app.Views
         public NavigationTree()
         {
             InitializeComponent();
+        }
+
+        private void TreeItem_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragDrop.DoDragDrop(this, new DataObject(), DragDropEffects.Copy | DragDropEffects.Move);
+
+            if (sender.GetType().Name == "StackPanel")
+                System.Console.WriteLine($"mouse left button down on {sender.GetType().Name}, {((sender as StackPanel).Children[1] as TextBlock).Text}");
+            else
+                System.Console.WriteLine($"mouse left button down on {sender.GetType().Name}");
+
+
         }
     }
 }
